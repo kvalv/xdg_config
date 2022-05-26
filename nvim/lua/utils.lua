41027b0b3acbb20f063bda2dfe5888446931f977
Mikael Kvalvaer <mikaelkvalvaer@gmail.com>
2022-05-26 13:14:27 +0200

    formatting

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,50 +1,56 @@
 local Path = require("plenary.path")
 
 local M = {}
+M.tuple = require("tuple")  -- re-export...
 
 M.reload = function(name)
-    require('plenary.reload').reload_module(name)
-    return require(name)
+	require("plenary.reload").reload_module(name)
+	return require(name)
 end
 _G.reload = M.reload
 
 -- some util functions...
 M.tbl_index = function(t, pred)
-    for index, v in ipairs(t) do
-        if pred(v) then
-            return index
-        end
-    end
-    return nil
+	for index, v in ipairs(t) do
+		if pred(v) then
+			return index
+		end
+	end
+	return nil
 end
 M.join = function(parts, sep)
-    local s = ""
-    for _, p in ipairs(parts) do
-        s = s .. p .. sep
-    end
-    return s:sub(1, -2)
+	local s = ""
+	for _, p in ipairs(parts) do
+		s = s .. p .. sep
+	end
+	return s:sub(1, -2)
 end
 
 M.partition = function(t, index)
-    local out = {}
-    for i = index, #t do
-        table.insert(out, t[i])
-    end
-    return out
+	local out = {}
+	for i = index, #t do
+		table.insert(out, t[i])
+	end
+	return out
 end
 
 M.log = require("plenary.log").new({
-    plugin = "mk",
-    level = "warn",
+	plugin = "mk",
+	level = "warn",
 })
+
+
+M.printf = function(s, ...)
+    return print(string.format(s, ...))
+end
 
 -- stolen from https://github.com/ThePrimeagen/refactoring.nvim/blob/master/lua/refactoring/tests/utils.lua
 function M.read_file(file)
-    return Path:new("lua", "refactoring", "tests", file):read()
+	return Path:new("lua", "refactoring", "tests", file):read()
 end
 
 function M.vim_motion(motion)
-    vim.cmd(string.format(':exe "norm! %s\\<esc>"', motion))
+	vim.cmd(string.format(':exe "norm! %s\\<esc>"', motion))
 end
 
 return M
